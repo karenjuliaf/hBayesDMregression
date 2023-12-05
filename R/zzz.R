5367c49c812c1c15ae2ed27b01d48e352164fc72
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2023-12-05 13:48:23 -0500

    Delete zzz.R

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-#' @noRd
-
-.onLoad <- function(libname, pkgname) {
-  # nocov start
-  if (FLAG_BUILD_ALL) {
-    modules <- paste0("stan_fit4", names(stanmodels), "_mod")
-    for (m in modules) loadModule(m, what = TRUE)
-  }
-} # nocov end
