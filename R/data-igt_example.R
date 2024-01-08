4495f940b40fee6631c23fb3637a71f491f9f6ef
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2024-01-08 17:29:25 -0500

    Create data-igt_example.R

---
+++ R/data-igt_example.R
@@ -0,0 +1,29 @@
+#' IGT example data
+#'
+#' This is an example data set to be used with the `igt_*_regression()` functions. It is an
+#' augmented version of the example IGT data set found in the `hBayesDM` package. Each subject in
+#' this data set performs 100 trials. As such, the values found in the columns containing the
+#' augmented covariates will be identical for all trials belonging to the same subject.
+#'
+#' @format A data frame with 400 observations and 14 variables:
+#' \describe{
+#' \item{subjID}{Subject ID}
+#' \item{trial}{Trial number}
+#' \item{choice}{The number of the deck chosen}
+#' \item{gain,loss}{The gain(s) and/or loss(es) associated with the choice}
+#' \item{age}{Age of the subject}
+#' \item{vmPFC}{Ventromedial prefrontal cortex}
+#' \item{vmPFC_area}{Area of the subject's vmPFC}
+#' \item{dlPFC}{Dorsolateral prefrontal cortex}
+#' \item{dlPFC_area}{Area of the subject's dorsolateral prefrontal cortex}
+#' \item{sex}{Binary indicator of the subject's sex; 0 = female, 1 = male}
+#' \item{psychosis1}{Binary indicator of the presence of something; 0 = no, 1 = yes}
+#' \item{psychosis2}{Binary indicator of the presence of something; 0 = no, 1 = yes}
+#' \item{psychosis3}{Binary indicator of the presence of something; 0 = no, 1 = yes}
+#' }
+#'
+#' @seealso The
+#'   \href{https://github.com/adamoshen/hBayesDMregression/data-raw/igt_example.R}{script} used to
+#'   create the igt_example data set.
+#'
+"igt_example"
